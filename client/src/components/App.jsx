2eed3c563666b8a727423da18e1d95302a6b1c8e
John Blaine <jablaine@uw.edu>
2018-03-15 08:53:26 -0700

    Init

---
+++ client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import $ from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>This works!</div>
+      );
+  }
+}
+
+export default App
