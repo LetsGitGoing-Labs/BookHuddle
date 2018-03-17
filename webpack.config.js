0510a484a8d43ba8c8bee04a4250e4d563570122
qsmith1 <smith.queenie@gmail.com>
2018-03-17 14:45:45 -0400

    Add react-router
    Move bundle and package-lock to gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
         include : SRC_DIR,
         loader : 'babel-loader',
         query : {
-          presets : ['react', 'es2015']
+          presets : ['react', 'es2015'],
+          plugins: ['syntax-dynamic-import']
         }
       }
     ]
