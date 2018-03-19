cd57388a835cec91080c1123ab7ea4a2cb759296
qsmith1 <smith.queenie@gmail.com>
2018-03-19 10:17:00 -0400

    Add collapse for navbar

--- client/src/components/main-navbar.jsx
+++ client/src/components/main-navbar.jsx
@@ -5,25 +5,28 @@ class MainNavbar extends React.Component {
   render() {
     return (
       <div>
-        <nav class="navbar navbar-expand-md navbar-light bg-light">
-          <a class="navbar-brand" href="/">BookHuddle</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-md navbar-light bg-light">
+          <a className="navbar-brand" href="/">BookHuddle</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
-              <Link to="/about" class="nav-link">Our Team</Link>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+              <Link to="/" className="nav-link">Home</Link>
               </li>
-              <li class="nav-item">
-                <Link to="/faq" class="nav-link">FAQ</Link>
+              <li className="nav-item">
+              <Link to="/about" className="nav-link">Our Team</Link>
               </li>
-              <li class="nav-item active">
-                <Link to="/signup" class="nav-link">Signup</Link>
+              <li className="nav-item">
+                <Link to="/faq" className="nav-link">FAQ</Link>
               </li>
-              <li class="nav-item">
-                <Link to="/login" class="nav-link">Login</Link>
+              <li className="nav-item active">
+                <Link to="/signup" className="nav-link">Signup</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/login" className="nav-link">Login</Link>
               </li>
             </ul>
   </div>
