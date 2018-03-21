52ee3a74a6205a88553879c03e4361675e0e683e
qsmith1 <smith.queenie@gmail.com>
2018-03-21 10:28:22 -0400

    Modify home page

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Book from './book.jsx';
+import MainNavbar from './main-navbar.jsx';
 
 class Home extends React.Component {
   constructor(props) {
@@ -9,88 +10,82 @@ class Home extends React.Component {
   smoothScroll() {
 
     $('html, body').animate({
-      scrollTop: $("#section-a").offset().top
-    }, 2000);
+      scrollTop: $("#how-it-works").offset().top
+    }, 1000);
   }
 
   render() {
     const bookList = [
-    { title: "The Pillars of the Earth", author: "Ken Follet", cover: "http://covers.openlibrary.org/b/isbn/0451207149-M.jpg"},
-    { title: "Where'd You Go Bernadette", author: "Maria Semple", cover: "http://covers.openlibrary.org/b/isbn/0316204277-M.jpg"},
-    {title: "The Help", author:"Kathryn Stockett", cover:"http://covers.openlibrary.org/b/isbn/0399155341-M.jpg"}
+      { title: "The Pillars of the Earth", author: "Ken Follet", cover: "http://covers.openlibrary.org/b/isbn/0451207149-M.jpg"},
+      { title: "Where'd You Go Bernadette", author: "Maria Semple", cover: "http://covers.openlibrary.org/b/isbn/0316204277-M.jpg"},
+      {title: "The Help", author:"Kathryn Stockett", cover:"http://covers.openlibrary.org/b/isbn/0399155341-M.jpg"}
     ]
     return (
       <div>
-      <header id="showcase" className="grid">
-        <div className='bg-image'></div>
-        <div className="content-wrapper">
-          <h1>BookHuddle</h1>
-          <p>Come together and share with a community who loves the same books you do.</p>
-          <button className='btn' onClick={this.smoothScroll.bind(this)}>Read More</button>
-        </div>
-      </header>
-      <main id='main'>
-        <section id='section-a' className='grid'>
-          <div className='content-wrapper'>
-            <h2 className="content-title">How it Works</h2>
+        <MainNavbar />
+        <header id="showcase" className="grid info-section">
+          <div className='bg-image'></div>
+          <div className="bg-overlay"></div>
+          <div className="content-wrapper">
+            <h1 className="showcase-header">BookHuddle</h1>
+            <p className="showcase-text pars">BookHuddle is an app for creating, joining and managing book clubs. Meet new friends who share your reading tastes, and through your book club, find the perfect next books to read!</p>
+            <button className='btn' onClick={this.smoothScroll.bind(this)}>Read More</button>
           </div>
-          <ul>
-            <li>
-              <div className="cardbox">
-                <i className="fas fa-user-circle"></i>
+        </header>
+        <main id='main'>
+          <section id='how-it-works' className='grid'>
+            <div className='content-wrapper'>
+              <h2 className="content-title">How it Works</h2>
+            <div className="content-wrapper info-cards">
+              <div className="card-box">
                 <div className="card-content">
                   <h3 className ="card-title">Create a free account</h3>
-                  <p>God victorious gains holiest aversion holiest depths evil reason.</p>
+                  <p>Sign up and start browsing book clubs by book genre or search by keywords or location. </p>
                 </div>
               </div>
-            </li>
-            <li>
-              <div className="cardbox">
-                <i className="fas fa-search"></i>
+              <div className="card-box">
                 <div className="card-content">
-                  <h3 className ="card-title">Find or create a club</h3>
-                  <p>Value selfish pious zarathustra burying convictions mountains sexuality abstract aversion horror.</p>
+                  <h3 className ="card-title">Create a club</h3>
+                  <p>Start your own club that you can make public or private. Pick what genres your club will be reading and add a description to help other readers find you.</p>
+                  </div>
+                </div>
+                <div className="card-box">
+                  <div className="card-content">
+                    <h3 className ="card-title">Get Connected</h3>
+                    <p>Stay up to date with your book club and join in the discussion using your club's message board. Get notified of upcoming meetings.</p>
+                  </div>
                 </div>
               </div>
-            </li>
-            <li>
-              <div className="cardbox">
-                <i className="fas fa-comments"></i>
-                <div className="card-content">
-                  <h3 className ="card-title">Participate in the discussion</h3>
-                  <p>Strong truth play aversion revaluation horror contradict sea good</p>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </section>
-
-        <section id="section-b" className="grid">
-          <div className='content-wrapper'>
-            <h2 className="content-title">Our Top Picks</h2>
-            <div className="carousel-list">
-              { bookList.map((book, index) => <Book key={index} book={book} /> ) }
             </div>
-          </div>
-        </section>
+          </section>
 
-        <section id='section-c' className='grid'>
-          <div className='box'>
-            <h2 className="content-title">What Others Are Saying</h2>
-            <p>Enlightenment self philosophy enlightenment of.</p>
-          </div>
-          <div className='box'>
-            <h2 className="content-title">Get Started Today</h2>
-            <p>Enlightenment self philosophy enlightenment of. Joy dead against intentions deceptions.</p>
-            <a href='#' className='btn'>Sign up</a>
-          </div>
-        </section>
-      </main>
+          <section id="section-b" className="grid book-carousel">
+            <div className='content-wrapper'>
+              <h2 className="content-title">Our Top Picks</h2>
+              <p><em>Popular books from our book clubs!</em></p>
+              <div className="carousel-list">
+                { bookList.map((book, index) => <Book key={index} book={book} /> ) }
+              </div>
+            </div>
+          </section>
 
-      <footer id='main-footer' className='grid'>
-        <div>Book Huddle</div>
-        <div>Project by LetsGetGoing</div>
-      </footer>
+          <section id='section-c' className='grid'>
+            <div className='box'>
+              <h2 className="content-title">What Others Are Saying</h2>
+              <p>Enlightenment self philosophy enlightenment of.</p>
+            </div>
+            <div className='box'>
+              <h2 className="content-title">Get in the Huddle</h2>
+              <p>Read your next classic with friends.</p>
+              <a href='#' className='btn'>Sign up</a>
+            </div>
+          </section>
+        </main>
+
+        <footer id='main-footer' className='grid'>
+          <p>Book Huddle</p>
+          <p>Project by LetsGetGoing</p>
+        </footer>
       </div>
     );
   }
