ccce2ce13f6212ee714720c1e915ad88c884e327
qsmith1 <smith.queenie@gmail.com>
2018-03-21 10:27:03 -0400

    Move main-navbar

--- client/src/components/main-navbar.jsx
+++ client/src/components/main-navbar.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 class MainNavbar extends React.Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand-md navbar-light bg-light">
-          <a className="navbar-brand" href="/">BookHuddle</a>
+          <Link to='/' className="navbar-brand">BookHuddle</Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-              <Link to="/" className="nav-link">Home</Link>
-              </li>
               <li className="nav-item">
               <Link to="/about" className="nav-link">Our Team</Link>
               </li>
