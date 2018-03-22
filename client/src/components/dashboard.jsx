b0142bd9f5c21ed33a992cddf82c749ae8678f8d
John Blaine <jablaine@uw.edu>
2018-03-22 10:34:54 -0700

    Beginning passing props to from dashboard to create-a-club

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -126,7 +126,9 @@ class Dashboard extends React.Component {
         <div>
           {/*<ClubListDashboard/>*/}
         </div>
-        <Route path='/create-club' component={ CreateClub } />
+        <Route path='/create-club' render={ (props) => {
+          <CreateClub {...props} userResponseData={this.props.location.state.userResponseData[0]} />
+        } } />
         <Route path='/profile' component={ Profile } />
       </div>
     );
