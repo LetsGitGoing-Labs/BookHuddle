163ed9aa75ac62263e52d8de629ff600f675475f
qsmith1 <smith.queenie@gmail.com>
2018-03-22 09:56:53 -0400

    change login for rebase merge

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -15,6 +15,7 @@ class Login extends React.Component {
       errMsg: '',
       userResponseData: ''
     };
+
     this.onChange = this.onChange.bind(this);
     this.checkLoginState = this.checkLoginState.bind(this);
     this.signupView = this.signupView.bind(this);
@@ -67,10 +68,8 @@ class Login extends React.Component {
           email: '',
           password: '',
           errMsg: 'User not found'});
-
       }
     });
-
   }
 
   render() {
@@ -81,28 +80,29 @@ class Login extends React.Component {
             state: { userResponseData: this.state.userResponseData }
             }} />)
     }
+
     return (
       <div>
-         {this.state.signup && <Signup />}
-         {!this.state.signup &&
-         <div  className="centerize">
-            <form>
-              <div className="form-group">
-                  <input type="email" className="form-control" id="inputEmail3" placeholder="Email" name="email" autoComplete="email" value={this.state.email} onChange={this.onChange}/>
-              </div>
-              <div className="form-group">
-                  <input type="password" className="form-control" id="login-password" autoComplete="current-password" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange}/>
-              </div>
+        {this.state.signup && <Signup />}
+        {!this.state.signup &&
+        <div  className="centerize">
+          <form>
+            <div className="form-group">
+              <input type="email" className="form-control" id="inputEmail3" placeholder="Email" name="email" autoComplete="email" value={this.state.email} onChange={this.onChange}/>
+            </div>
+            <div className="form-group">
+              <input type="password" className="form-control" id="login-password" placeholder="Password" name="password" autoComplete="current-password" value={this.state.password} onChange={this.onChange}/>
+            </div>
               {this.state.errMsg.length > 1 && <div id="login-err"><p>{this.state.errMsg}</p></div>}
-              <div className="form-group">
-                <Link to='/dashboard'>
-                  <button id="button1" type="submit" onClick={this.handleSubmit}>Login</button>
-                </Link>
-                <a href="/auth/facebook" className="btn btn-primary"><span className="fa fa-facebook"></span> Facebook</a>
-              </div>
-            </form>
-          </div>}
-        </div>
+            <div className="form-group">
+              <Link to='/dashboard'>
+                <button id="button1" type="submit" onClick={this.handleSubmit}>Login</button>
+              </Link>
+              <a href="/auth/facebook" className="btn btn-primary"><span className="fa fa-facebook"></span> Facebook</a>
+            </div>
+          </form>
+        </div>}
+      </div>
     );
   }
 }
