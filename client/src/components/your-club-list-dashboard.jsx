014c8be8a733cd7ea66ff471c64a477d19887943
qsmith1 <smith.queenie@gmail.com>
2018-03-22 21:18:19 -0400

    Clean up club-list-dashboard code

--- client/src/components/your-club-list-dashboard.jsx
+++ client/src/components/your-club-list-dashboard.jsx
@@ -7,23 +7,18 @@ class YourClubListDashboard extends React.Component {
     super(props);
   }
 
-  componentWillReceiveProps(nextProps) {
-  }
-
-    render() {
-      return !this.props.yourClubList || this.props.yourClubList.length === 0 ?
-        (
-          <div className="ui segment">
-            <h2 className="dashboard-header">Your Book Clubs:</h2>
-            <div>You're not in any book clubs!</div>
-          </div>
-        )
-      :
-    (
-      <div className="ui segment">
-        <h2 className="dashboard-header">Your Book Clubs:</h2>
-        <div className="ui seven link cards">
-          {this.props.yourClubList.map((yourClub, i) =>
+  render() {
+    return !this.props.yourClubList || this.props.yourClubList.length === 0 ?
+      (
+        <div className="ui segment">
+          <h2 className="dashboard-header">Your Book Clubs:</h2>
+          <div>You're not in any book clubs!</div>
+        </div>
+      ) : (
+        <div className="ui segment">
+          <h2 className="dashboard-header">Your Book Clubs:</h2>
+          <div className="ui seven link cards">
+            {this.props.yourClubList.map((yourClub, i) =>
             <Link to='/club' onClick={this.props.renderClub}>
               <div className="card" id={i} style={{padding: '1em'}} >
                 <img className="carousel-card-img" src={yourClub.image}/>
