4f373b90e9d5a798eae5346ea3673ef80fbc0428
qsmith1 <smith.queenie@gmail.com>
2018-03-27 14:55:38 -0400

    Simplify club component data

--- client/src/components/clubs/club.jsx
+++ client/src/components/clubs/club.jsx
@@ -14,10 +14,10 @@ class Club extends React.Component {
     const clubData = this.props.clubs.find((ind) => ind.id === this.props.match.params.clubId)
     return (
       <div id="club-page" className="col-md-9">
-      <div className="container">
-          <h2 className="clubHeader">{clubData.title}</h2>
+        <div className="container">
+          <h2 className="club-title">{clubData.title}</h2>
           <div>
-            <nav id="club-page-nav">
+            <nav id="club-page-nav" className="centered">
               <ul>
                 <li><Link to="#">Upcoming</Link></li>
                 <li><Link to="#">Members</Link></li>
@@ -33,15 +33,15 @@ class Club extends React.Component {
             </div>
               {this.state.meetings.map((meeting) =>
                 <div className="row meeting-content">
-                  <div className="col-md-4">
+                  <div className="col-md-3">
                     <img className="book-cover" src={meeting.meeting_book.imgSrc}/>
                   </div>
-                  <div key={meeting.id} className="meeting-text col-md-8">
+                  <div key={meeting.id} className="meeting-text col-md-9">
+                    <p>{meeting.meeting_topic}</p>
+                    <p>{meeting.meeting_description}</p>
                     <p>{meeting.meeting_date} @ {meeting.meeting_time}</p>
-                    <p>{meeting.meeting_book.author}'s {meeting.meeting_book.title}</p>
                     <p>Hosted by {meeting.meeting_host}</p>
                     <p>{meeting.meeting_street_address}</p>
-                    <p>{meeting.meeting_notes}</p>
                   </div>
 
                 </div>
@@ -61,40 +61,12 @@ const meetings = [
         meeting_date: 'March 30th, 2018',
         meeting_time: '7:30 PM',
         meeting_host: 'joey@gmail.com',
+        meeting_topic: 'Finishing Persuasion',
         meeting_street_address: '123 Main Street, Houston, TX 12345',
-        meeting_notes: 'Hey everyone! Same place as usual.  We\'ll be finishing our discussion of Persuasion.  It\'s Bob\'s turn to bring refreshments.  I\'ll email the discussion questions the day of.',
+        meeting_description: 'This will be the last meeting to wrap up Jane Austen\'s Persuasion',
         meeting_book: {
           amazon_id: null,
           title: 'Persuasion',
           author: 'Jane Austen',
           imgSrc: 'https://images-na.ssl-images-amazon.com/images/I/41uM9MBn1CL._SX326_BO1,204,203,200_.jpg'
-        }
-      },
-      {
-        id: 2,
-        meeting_date: 'April 1st, 2018',
-        meeting_time: '7:30 PM',
-        meeting_host: 'joey@gmail.com',
-        meeting_street_address: '123 Main Street, Houston, TX 12345',
-        meeting_notes: 'Next month we\'re reading Sense and Sensibility.  Amy and Stan are bringing refreshments.',
-        meeting_book: {
-          amazon_id: null,
-          title: 'Sense and Sensibility',
-          author: 'Jane Austen',
-          imgSrc: 'https://images-na.ssl-images-amazon.com/images/I/414hu6Q4xFL._SX324_BO1,204,203,200_.jpg'
-        }
-      },
-      {
-        id: 3,
-        meeting_date: 'May 3rd, 2018',
-        meeting_time: '10 am',
-        meeting_host: 'joey@gmail.com',
-        meeting_street_address: 'TBD',
-        meeting_notes: 'For May we need someone to volunteer to host the meeting.  We\'ll be done with Persuasion, Thinking of reading .',
-        meeting_book: {
-          amazon_id: null,
-          title: 'Mansfield park',
-          author: 'Jane Austen',
-          imgSrc: 'https://images-na.ssl-images-amazon.com/images/I/41jTnx6I%2BbL._SX324_BO1,204,203,200_.jpg'
-        }
       }]
