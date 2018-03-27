8ab5256bdc2a11b7a5bbd10ac261db79ad2bf4f1
qsmith1 <smith.queenie@gmail.com>
2018-03-27 11:20:40 -0400

    Add style for sidebar

--- client/src/components/navigation/sidebar.jsx
+++ client/src/components/navigation/sidebar.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <div id="sidebar"class="col-md-3">
+    <div id="sidebar"className="col-md-3">
       <div className="sidebar-content">
-        <p class="centered"><a href="profile.html"><img src="http://source.unsplash.com/aZm98bjnA20" className="img-circle"/></a></p>
-        <h6 className="centered">Joe Smalls</h6>
-        <ul className="centered">
+        <p className="centered"><a href="profile.html"><img src="http://source.unsplash.com/aZm98bjnA20" className="img-circle"/></a></p>
+        <h5 className="centered">Jane Smith</h5>
+        <h6 className="centered" style={{color: 'rgb(174, 178, 183)'
+        }}><em>New York, NY</em></h6>
+        <ul className="mt">
           <li>
-            <a href="#">Dashboard</a>
+            <Link to="/dashboard">
+              <i className="fas fa-desktop"></i><span>Dashboard</span>
+            </Link>
           </li>
           <li>
-            <a href="#">Create a Club</a>
+            <Link to="/dashboard/create-club">
+              <i className="fas fa-plus-circle"></i><span>New Club</span>
+            </Link>
           </li>
           <li>
-            <a href="#">Logout</a>
+            <Link to="/logout"><i className="fas fa-sign-out-alt">
+              </i><span>Logout</span>
+            </Link>
           </li>
         </ul>
       </div>
