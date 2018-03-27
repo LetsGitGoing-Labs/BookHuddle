1dfe6e196ce21944f24e0b53186f935e287e44cf
qsmith1 <smith.queenie@gmail.com>
2018-03-27 13:47:40 -0400

    Build out meeting-card

--- client/src/components/dashboard/panel-meetings.jsx
+++ client/src/components/dashboard/panel-meetings.jsx
@@ -15,9 +15,27 @@ const MeetingsPanel = (props) => (
         <h3>UPCOMING MEETINGS</h3>
         {props.meetings.map((meeting, id) =>
           (
-            <div key={id}>
-              <h5>{meeting.meeting_date + ' at ' + meeting.meeting_time}</h5>
-              <p>{meeting.meeting_street_address}</p>
+            <div key={id} class="panel-content">
+              <div class="row">
+                <div class="col-md-3">
+                  <div class="date-panel">
+                    <p>Monday</p>
+                    <h2>24</h2>
+                    <p>March</p>
+                    <h6>9:00 AM</h6>
+                  </div>
+                </div>
+                <div class="col-md-9">
+                  <div class="panel-body">
+                    <h4>Finishing Persuasion</h4>
+                    <p><em>This will be the last meeting to wrap up Jane Austen's Persuation.</em></p>
+                    <p>Host by James Brown</p>
+                  </div>
+                  <div class="panel-button">
+                    <button className="btn btn-danger">See Details</button>
+                  </div>
+                </div>
+              </div>
             </div>
           )
         )}
