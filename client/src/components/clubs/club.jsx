4763aea86283e8dc9cdfb3827f2607128c0d15ee
qsmith1 <smith.queenie@gmail.com>
2018-03-28 11:56:45 -0400

    Club page renders club info

--- client/src/components/clubs/club.jsx
+++ client/src/components/clubs/club.jsx
@@ -10,21 +10,18 @@ import '../../styles/club.css';
 class Club extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {
-      meetings: meetings
-    };
   }
 
   render () {
-    const clubData = this.props.clubs.find((ind) => ind.id === this.props.match.params.clubId)
+    const clubData = this.props.clubs.find((club) => club.id === this.props.match.params.clubId)
     return (
       <div id="club-page" className="col-md-9">
         <div className="container">
-          <h2 className="club-title">{clubData.title}</h2>
+          <h2 className="club-title">{clubData.club_name}</h2>
             <ClubPageNavbar />
             <div class="tab-content" id="nav-tabContent">
               <div class="tab-pane fade show active" id="nav-upcoming" role="tabpanel" aria-labelledby="nav-home-tab">
-              <UpcomingMeetings meetings={this.state.meetings}/>
+              <UpcomingMeetings meetings={clubData.meetings}/>
               </div>
               <div class="tab-pane fade" id="nav-members" role="tabpanel" aria-labelledby="nav-members-tab">
                 Members
@@ -43,21 +40,3 @@ class Club extends React.Component {
 }
 
 export default Club;
-
-const meetings = [
-      {
-        id: 1,
-        meeting_date: 'March 30th, 2018',
-        meeting_time: '7:30 PM',
-        meeting_host: 'joey@gmail.com',
-        meeting_topic: 'Finishing Persuasion',
-        meeting_street_address: '123 Main Street, Houston, TX 12345',
-        meeting_description: 'This will be the last meeting to wrap up Jane Austen\'s Persuasion',
-        meeting_book: {
-          amazon_id: null,
-          title: 'Persuasion',
-          author: 'Jane Austen',
-          imgSrc: 'https://images-na.ssl-images-amazon.com/images/I/41uM9MBn1CL._SX326_BO1,204,203,200_.jpg'
-        }
-      }
-    ];
