96293bb4bb775ee26790d7172781c1f16956614c
qsmith1 <smith.queenie@gmail.com>
2018-03-30 17:06:30 -0400

    reconfigure webpack config to reduce app size

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
 
 const SRC_DIR = path.join(__dirname, '/client/src');
@@ -27,4 +28,12 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new UglifyJSPlugin()
+  ]
 };
