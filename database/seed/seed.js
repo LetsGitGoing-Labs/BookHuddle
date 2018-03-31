12de232c90841dae2b9aef26a2aa4002249e5b2f
Michael Butak <mikebutak@gmail.com>
2018-03-30 22:01:45 -0500

    create file structure for seeding db

---
+++ database/seed/seed.js
@@ -0,0 +1,11 @@
+const db = require('./database/index.js');
+let sampleUserData = require('./sampleData.json');
+
+db.dropDatabase();
+
+sampleUserData.forEach((obj, err) => {
+  db.saveUser(obj, (err, data) => {
+    if (err) { console.log('error saving pet owner to database', err); }
+    else { console.log('saved pet owner to the database', data); }
+  });
+});
