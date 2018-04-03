d3913859a6cc956ea8964cf0a14e7f27a1938bf9
qsmith1 <smith.queenie@gmail.com>
2018-04-03 16:54:26 -0400

    Modify and style club and meeting navbars

--- client/src/components/navigation/club-nav.jsx
+++ client/src/components/navigation/club-nav.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ClubPageNavbar = () => (
   <nav className="centered">
-    <div className="nav" id="club-page-nav" role="tablist">
+    <div className="nav page-nav" id="club-page-nav" role="tablist">
       <a className="nav-link active" id="nav-upcoming-tab" data-toggle="tab" href="#nav-upcoming" role="tab" aria-controls="nav-upcoming" aria-selected="true">Upcoming</a>
       <a className="nav-link" id="nav-members-tab" data-toggle="tab" href="#nav-members" role="tab" aria-controls="nav-members" aria-selected="false">Members</a>
       <a className="nav-link" id="nav-history-tab" data-toggle="tab" href="#nav-history" role="tab" aria-controls="nav-history" aria-selected="false">Book History</a>
--- client/src/components/navigation/meeting-nav.jsx
+++ client/src/components/navigation/meeting-nav.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MeetingPageNavbar = () => (
   <nav className="centered">
-    <div className="nav" id="meeting-page-nav" role="tablist">
+    <div className="nav page-nav" id="meeting-page-nav" role="tablist">
       <a className="nav-link active" id="nav-details-tab" data-toggle="tab" href="#nav-details" role="tab" aria-controls="nav-details" aria-selected="true">Details</a>
       <a className="nav-link" id="nav-meet-members-tab" data-toggle="tab" href="#nav-meet-members" role="tab" aria-controls="nav-meet-members" aria-selected="false">Members</a>
       <a className="nav-link" id="nav-trivia-tab" data-toggle="tab" href="#nav-trivia" role="tab" aria-controls="nav-trivia" aria-selected="false">Trivia</a>
