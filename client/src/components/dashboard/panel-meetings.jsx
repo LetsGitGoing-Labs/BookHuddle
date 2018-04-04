ea9e8e05602b8477e3c7ec01db52f0f3f07f4f44
qsmith1 <smith.queenie@gmail.com>
2018-04-03 20:41:03 -0400

    Refactor meetings-panel to use meeting-list

--- client/src/components/dashboard/panel-meetings.jsx
+++ client/src/components/dashboard/panel-meetings.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { DateTime } from 'luxon';
-
-const NoMeetings = () => (
-  <div id="meetings-list" className="panel-card">
-    <h3>UPCOMING MEETINGS</h3>
-    <div>No meetings yet!</div>
-  </div>
-);
+import MeetingsList from '../meetings/meeting-list.jsx';
 
 class MeetingsPanel extends React.Component {
   constructor(props) {
@@ -15,10 +9,14 @@ class MeetingsPanel extends React.Component {
     this.state = {
       meetings: [],
     }
-    this.meetingDate = this.meetingDate.bind(this);
+    this.filterMeetings = this.filterMeetings.bind(this);
   }
 
-  componentDidMount() {
+  componentWillMount() {
+    this.filterMeetings();
+  }
+
+  filterMeetings() {
     const now = DateTime.local().ts;
     const list = this.props.clubs[0].meetings
       .filter((meeting) => (
@@ -32,39 +30,12 @@ class MeetingsPanel extends React.Component {
     });
   }
 
-  meetingDate(timestamp) {
-    return DateTime.fromISO(timestamp).toFormat('yyyy LLL dd t');
-  }
-
   render() {
     return(
-      !this.state.meetings || this.state.meetings.length === 0 ?
-        <NoMeetings />
-      : (
-        <div id="meetings-list" className="panel-card">
-          <h3>UPCOMING MEETINGS</h3>
-          {this.state.meetings.map((meeting, id) => (
-            <div key={id} className="panel-content">
-              <div className="row">
-                <div className="col-md-3">
-                  <img className="meeting-book" src="http://covers.openlibrary.org/b/isbn/0451207149-M.jpg" />
-                </div>
-                <div className="col-md-9">
-                  <div className="panel-body">
-                    <h4>{meeting.meeting_name}</h4>
-                    <p><em>{this.meetingDate(meeting.meeting_timestamp)}</em></p>
-                    <p>hosted by {meeting.meeting_host}</p>
-                    <p><em>{meeting.meeting_notes}</em></p>
-                  </div>
-                  <div className="panel-button">
-                    <Link to={`dashboard/${meeting.club_id}/${meeting.id}`} ><button className="btn btn-danger">See Details</button></Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      )
+      <div id="meetings-list" className="panel-card">
+        <h3>UPCOMING MEETINGS</h3>
+        <MeetingsList root='dashboard/' meetings={this.state.meetings}/>
+      </div>
     )
   }
 }
