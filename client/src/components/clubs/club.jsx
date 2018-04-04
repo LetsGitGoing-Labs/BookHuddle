dd1ffe9da40c445d9784d1251c65d6310fb23a97
qsmith1 <smith.queenie@gmail.com>
2018-04-03 20:39:18 -0400

    Add filterMeetings and getClubData methods to club page'

--- client/src/components/clubs/club.jsx
+++ client/src/components/clubs/club.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { DateTime } from 'luxon';
 
 import UpcomingMeetings from './upcoming.jsx';
 import CreateMeeting from './create-meeting.jsx';
@@ -11,11 +12,37 @@ import '../../styles/club.css';
 
 class Club extends React.Component {
   constructor(props) {
-    super(props); this.state = {
+    super(props);
+    this.state = {
       joinMsg: '',
+      clubData: '',
+      pastMeetings: '',
+      futureMeetings: '',
     };
 
     this.userJoinClub = this.userJoinClub.bind(this);
+    this.getClubData = this.getClubData.bind(this);
+    this.filterMeetings = this.filterMeetings.bind(this);
+  }
+
+  componentDidMount() {
+    this.getClubData((clubData) => {
+      this.filterMeetings(clubData);
+    });
+  }
+
+  getClubData(cb) {
+    let clubData;
+    if (this.props.searchResults.length) {
+      clubData = this.props.searchResults.find(club => club.id === Number(this.props.match.params.clubId));
+    } else {
+      clubData = this.props.clubs.find(club => club.id === Number(this.props.match.params.clubId));
+    }
+    this.setState({
+      clubData
+    });
+
+    cb(clubData);
   }
 
   userJoinClub() {
@@ -47,31 +74,38 @@ class Club extends React.Component {
     });
   }
 
-  render() {
-    let clubData;
-    if (this.props.searchResults.length) {
-      clubData = this.props.searchResults.find(club => club.id === Number(this.props.match.params.clubId));
-    } else {
-      clubData = this.props.clubs.find(club => club.id === Number(this.props.match.params.clubId));
-    }
+  filterMeetings(clubData) {
+    const now = DateTime.local().ts;
+    const pastList = clubData.meetings.filter((meeting) => (
+      DateTime.fromISO(meeting.meeting_timestamp).valueOf() < now
+    ));
+    const futureList = clubData.meetings
+      .filter((meeting) => (
+        DateTime.fromISO(meeting.meeting_timestamp).valueOf() > now
+      ))
+      .sort((a,b) => (
+        DateTime.fromISO(a.meeting_timestamp).valueOf() - DateTime.fromISO(b.meeting_timestamp).valueOf()
+      ));
+    this.setState({
+      pastMeetings: pastList,
+      futureMeetings: futureList,
+    });
+  }
 
-    const showJoin = this.props.user.clubs.indexOf(clubData) >= 0 ? { display: 'none'} : { display: 'normal'}
+  render() {
+    const showJoin = this.props.user.clubs.indexOf(this.state.clubData) >= 0 ? { display: 'none'} : { display: 'normal'}
 
     return (
       <div id="club-page" className="col-md-9">
         <div className="container">
-          <h2 className="club-title">{clubData.club_name}</h2>
+          <h2 className="club-title">{this.state.clubData.club_name}</h2>
           <ClubPageNavbar />
           <div className="tab-content" id="nav-tabContent">
-            <div className="tab-pane fade show active" id="nav-upcoming" role="tabpanel" aria-labelledby="nav-home-tab">
-              <UpcomingMeetings meetings={clubData.meetings} />
-            </div>
+            <UpcomingMeetings futureMeetings={this.state.futureMeetings} />
             <div className="tab-pane fade" id="nav-members" role="tabpanel" aria-labelledby="nav-members-tab">
               <Members />
             </div>
-            <div className="tab-pane fade" id="nav-history" role="tabpanel" aria-labelledby="nav-history-tab">
-                <BookHistory meetings={clubData.meetings}/>
-            </div>
+            <BookHistory pastMeetings={this.state.pastMeetings} />
             <button style={showJoin} className="btn" onClick={this.userJoinClub}>Join Club</button>
             {this.state.joinMsg.length > 1 && <div id="join-msg"><p>{this.state.joinMsg}</p></div>}
             <div className="tab-pane fade" id="nav-create-meeting" role="tabpanel" aria-labelledby="nav-create-meeting-tab">
